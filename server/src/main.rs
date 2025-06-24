d475c45349e2fd8d73bac2eca92a655b980cd76f
Austin Delic <delicaustin0@gmail.com>
2025-06-23 18:16:37 -0700

    Revert "feat: enhance static file serving with fallback to index.html"
    This reverts commit 20a5e41f6035d0852082989edfa91a6c25a0c6f4.

--- server/src/main.rs
+++ server/src/main.rs
@@ -12,7 +12,6 @@ use std::{
 };
 use tokio::net::TcpListener;
 use tower_http::services::ServeDir;
-use tower_http::services::ServeFile;
 
 // === Shared number range tracker ===
 type SharedCounter = Arc<Mutex<u64>>;
@@ -51,9 +50,7 @@ async fn main() {
     let app = Router::new()
         .nest("/api", api_router)
         // Serve static files for all other routes (SPA)
-        .fallback_service(
-            ServeDir::new(DIST).not_found_service(ServeFile::new(format!("{}/index.html", DIST))),
-        );
+        .fallback_service(ServeDir::new(DIST));
 
     // Start the server
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
